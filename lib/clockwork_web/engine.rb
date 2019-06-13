fda6a112d075cc115230b04ce26db667e46b5a53
Andrew Kane <andrew@chartkick.com>
2019-06-12 22:16:51 -0700

    Fixed lint warnings

--- lib/clockwork_web/engine.rb
+++ lib/clockwork_web/engine.rb
@@ -2,7 +2,7 @@ module ClockworkWeb
   class Engine < ::Rails::Engine
     isolate_namespace ClockworkWeb
 
-    initializer "clockwork_web" do |app|
+    initializer "clockwork_web" do
       ClockworkWeb.clock_path ||= Rails.root.join("clock")
       require ClockworkWeb.clock_path.to_s
     end
