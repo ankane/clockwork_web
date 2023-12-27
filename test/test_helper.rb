6fa475c5b2753e88d91b938330c6478fc80fb2a8
Andrew Kane <andrew@ankane.org>
2023-12-27 12:47:59 -0500

    Added config.load_defaults to test setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,4 +5,6 @@ require "minitest/autorun"
 require "minitest/pride"
 
 Combustion.path = "test/internal"
-Combustion.initialize! :action_controller
+Combustion.initialize! :action_controller do
+  config.load_defaults Rails::VERSION::STRING.to_f
+end
